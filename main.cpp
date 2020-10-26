60bd4562d045ef8135f8cf90030d75050382e793
ed4st <61949882+ed4st@users.noreply.github.com>
2020-10-25 22:18:20 -0500

    some information added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+/*
+*    Author: Edgar Baquero
+*
+*    Matrix algebra: This code is made with final purpose of 
+*                    compute basic matrix algebra via cpp
+*                    implementation.
+*
+*    If you are on a Linux OS based system, you can compile and run via
+*    following command in terminal:
+*    >> g++ *.cpp -o exe && ./exe
+*
+*/
+
 #include "Matrix.h"
 
 int main(){
