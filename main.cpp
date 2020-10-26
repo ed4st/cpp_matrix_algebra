fec4a854256eb8de79280dddba5dfa92dd5439a9
ed4st <61949882+ed4st@users.noreply.github.com>
2020-10-25 21:11:01 -0500

    Creating a main-menu

--- main.cpp
+++ main.cpp
@@ -41,6 +41,63 @@ int main(){
     
 
     delete m2;
+
+    // Menu
+    int option = 0;
+    do
+    {
+        cout << "Enter one of the following options:\n";
+
+        cout << "   1. Sum two matrices.\n";
+        cout << "   2. Subtract two matrices.\n";
+        cout << "   3. Product of two matrices.\n";
+        cout << "   4. Traspose a matrix.\n";
+        cout << "   5. Inverse of a matrix.\n";
+        cout << "   0. Exit.\n";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            int m1, n1, m2, n2;
+            cout << "Enter the number of rows of matrix 1: ";
+            cin >> m1;
+            cout << endl;
+            cout << "Enter the number of columns of matrix 1: ";
+            cin >> n1;
+            cout << endl;
+            cout << "Enter the number of rows of matrix 2: ";
+            cin >> m2;
+            cout << endl;
+            cout << "Enter the number of columns of matrix 2: ";
+            cin >> n2;
+            cout << endl;
+
+            //creating matrices
+            Matrix *M1 = new Matrix(m1,n1);
+            Matrix *M2 = new Matrix(m2,n2);
+            
+            //filling matrix randomly
+            M1->random_matrix();
+            M2->random_matrix();
+
+            cout << "*********Following matrices are randomly generated*********\n"
+            cout << "---------Matrix 1---------\n"
+            M1->print();
+            cout << "---------Matrix 2---------\n"
+            M2->print();
+            cout << "---------Sum---------\n"
+            (*M1 + *M2)->print();
+            break;
+        
+        default:
+            break;
+        }
+
+
+    } while (option != 0);
+    
+    
+
     return 0;
 }
 //compilación por terminal
