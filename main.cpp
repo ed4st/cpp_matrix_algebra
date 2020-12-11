f1a04d861a8afce0128b49072b93f8fe62a0928a
Edgar Baquero <e_baquero@hotmail.com>
2020-12-10 23:50:30 -0500

    report finished

--- main.cpp
+++ main.cpp
@@ -16,25 +16,182 @@
 #include <cmath>
 #include <iostream>
 #include <pthread.h>
+#include <fstream> //save the time execution of each program
+#include <chrono> //measures the time of a function
+using namespace std::chrono; 
+void timeit_sum(){
+    ofstream sum_records;
+
+    int sizes[50] = {50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,1300,1350,1400,1450,1500,1550,1600,1650,1700,1750,1800,1850,1900,1950,2000,2050,2100,2150,2200,2250,2300,2400,2500,3000,5000};
+    sum_records.open ("sum_records.csv");
+    sum_records<<"size, seq_time, par_time\n";
+    for (int i = 0; i < 50; i++) {
+        
+        int r = rand()%100;
+        Matrix *M1 = new Matrix(sizes[i],sizes[i]);
+        Matrix *M2 = new Matrix(sizes[i],sizes[i]);
+        sum_records<<sizes[i]<<",";
+        srand(time(NULL));
+        M1->random_matrix();
+        M2->random_matrix();
+
+        // Get starting timepoint 
+        auto start1 = high_resolution_clock::now(); 
+        (*M1 + *M2);
+        auto stop1 = high_resolution_clock::now(); 
+        auto duration1 = duration_cast<microseconds>(stop1 - start1);
+        double time1 = duration1.count();
+        sum_records<<time1<<",";
+
+
+        // Get starting timepoint 
+        auto start2 = high_resolution_clock::now(); 
+        parallel_sum(M1,M2);
+        auto stop2 = high_resolution_clock::now(); 
+        auto duration2 = duration_cast<microseconds>(stop2 - start2);
+        double time2 = duration2.count();
+        sum_records<<time2<<"\n";
+        
+        
+        delete M1, M2;
+    }
+}
+
+void timeit_sub(){
+    ofstream sum_records;
+
+    int sizes[50] = {50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1050,1100,1150,1200,1250,1300,1350,1400,1450,1500,1550,1600,1650,1700,1750,1800,1850,1900,1950,2000,2050,2100,2150,2200,2250,2300,2400,2500,3000,5000};
+    sum_records.open ("sub_records.csv");
+    sum_records<<"size, seq_time, par_time\n";
+    for (int i = 0; i < 50; i++) {
+        
+        int r = rand()%100;
+        Matrix *M1 = new Matrix(sizes[i],sizes[i]);
+        Matrix *M2 = new Matrix(sizes[i],sizes[i]);
+        sum_records<<sizes[i]<<",";
+        srand(time(NULL));
+        M1->random_matrix();
+        M2->random_matrix();
+
+        // Get starting timepoint 
+        auto start1 = high_resolution_clock::now(); 
+        (*M1 - *M2);
+        auto stop1 = high_resolution_clock::now(); 
+        auto duration1 = duration_cast<microseconds>(stop1 - start1);
+        double time1 = duration1.count();
+        sum_records<<time1<<",";
+
+
+        // Get starting timepoint 
+        auto start2 = high_resolution_clock::now(); 
+        parallel_sub(M1,M2);
+        auto stop2 = high_resolution_clock::now(); 
+        auto duration2 = duration_cast<microseconds>(stop2 - start2);
+        double time2 = duration2.count();
+        sum_records<<time2<<"\n";
+        
+        
+        delete M1, M2;
+    }
+}
+
+
+void timeit_mul(){
+    ofstream sum_records;
+
+    int sizes[50] = {10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90 , 100 , 110 , 120 , 130 , 140 , 150 , 160 , 170 , 180 , 190 , 200 , 210 , 220 , 230 , 240 , 250 , 260 , 270 , 280 , 290 , 300 , 310 , 320 , 330 , 340 , 350 , 360 , 370 , 380 , 390 , 400 , 410 , 420 , 430 , 440 , 450 , 460 , 470 , 480 , 490, 500};
+    sum_records.open ("mul_records.csv");
+    sum_records<<"size, seq_time, par_time\n";
+    for (int i = 0; i < 50; i++) {
+        
+        int r = rand()%100;
+        Matrix *M1 = new Matrix(sizes[i],sizes[i]);
+        Matrix *M2 = new Matrix(sizes[i],sizes[i]);
+        sum_records<<sizes[i]<<",";
+        srand(time(NULL));
+        M1->random_matrix();
+        M2->random_matrix();
+
+        // Get starting timepoint 
+        auto start1 = high_resolution_clock::now(); 
+        (*M1 * *M2);
+        auto stop1 = high_resolution_clock::now(); 
+        auto duration1 = duration_cast<microseconds>(stop1 - start1);
+        double time1 = duration1.count();
+        sum_records<<time1<<",";
+
+
+        // Get starting timepoint 
+        auto start2 = high_resolution_clock::now(); 
+        parallel_mul(M1,M2);
+        auto stop2 = high_resolution_clock::now(); 
+        auto duration2 = duration_cast<microseconds>(stop2 - start2);
+        double time2 = duration2.count();
+        sum_records<<time2<<"\n";
+        
+        
+        delete M1, M2;
+    }
+}
+
+
+
+void timeit_inv(){
+    ofstream sum_records;
+
+    int sizes[50] = {10 , 20 , 30 , 40 , 50 , 60 , 70 , 80 , 90 , 100 , 110 , 120 , 130 , 140 , 150 , 160 , 170 , 180 , 190 , 200 , 210 , 220 , 230 , 240 , 250 , 260 , 270 , 280 , 290 , 300 , 310 , 320 , 330 , 340 , 350 , 360 , 370 , 380 , 390 , 400 , 410 , 420 , 430 , 440 , 450 , 460 , 470 , 480 , 490, 500};
+    sum_records.open ("inv_records.csv");
+    sum_records<<"size, seq_time, par_time\n";
+    for (int i = 0; i < 50; i++) {
+        
+        Matrix *M = new Matrix(sizes[i],sizes[i]);
+        sum_records<<sizes[i]<<",";
+        srand(time(NULL));
+        M->random_matrix();
+
+        // Get starting timepoint 
+        auto start1 = high_resolution_clock::now(); 
+        M->inverse();
+        auto stop1 = high_resolution_clock::now(); 
+        auto duration1 = duration_cast<microseconds>(stop1 - start1);
+        double time1 = duration1.count();
+        sum_records<<time1<<",";
+
+
+        // Get starting timepoint 
+        auto start2 = high_resolution_clock::now(); 
+        parallel_inv(M);
+        auto stop2 = high_resolution_clock::now(); 
+        auto duration2 = duration_cast<microseconds>(stop2 - start2);
+        double time2 = duration2.count();
+        sum_records<<time2<<"\n";
+        
+        
+        delete M;
+    }
+}
 
 
 int main(){
-    
-    int nrows, ncols;
-    nrows = 2;
-    ncols = 2;
+    timeit_sum();
+    timeit_sub();
+    timeit_mul();
+    timeit_inv();
+
     //creating matrices
-    Matrix *M1 = new Matrix(nrows,ncols);
-    Matrix *M2 = new Matrix(nrows,ncols);
+    //Matrix *M1 = new Matrix(nrows,ncols);
+    //Matrix *M2 = new Matrix(nrows,ncols);
     
 
     //filling matrix randomly
     //srand(time(NULL));
-    M1->random_matrix();
-    M2->random_matrix();
+    
 
 
-    M1->print();
+    //M1->print();
+
+    
+
     /*M2->print();
     cout <<"suma:\n";
     (*M1 + *M2)->print();    
@@ -44,12 +201,12 @@ int main(){
     parallel_sub(M1,M2)->print();
     cout <<"Multiplicacion:\n";
     (*M1 * *M2)->print();    
-    parallel_mul(M1,M2)->print();*/
+    parallel_mul(M1,M2)->print();
 
     M1->inverse()->print();
-    parallel_inv(M1)->print();
+    parallel_inv(M1)->print();*/
 
-    delete M1, M2;
+    //delete M1, M2;
 
 
 
